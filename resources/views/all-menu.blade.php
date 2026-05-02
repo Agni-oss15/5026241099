594757c4363aa34956418eef32d2d11dea804de5
Agni-oss15 <naufal.umma@gmail.com>
2026-05-02 16:03:20 +0700

    tugas pertemuan 7

--- resources/views/all-menu.blade.php
+++ resources/views/all-menu.blade.php
@@ -14,24 +14,13 @@
 
 <body>
     <div class="container">
-        <p></p>
         <div class="before-header logo-bawah py-4 ">
-            <a href="http://linktr.ee/" target="_blank" style="width: 5%;">
-                <img src="{{ asset('linktree-asset/tree-deciduous.png') }}" class="logo-atas" alt="LinkTree">
-            </a>
-            <a href="https://linktr.ee/Agni.Graph" target="_blank" style="width: 5%;"><img src="{{ asset('linktree-asset/screen-share.png') }}"
-                    class="logo-atas" alt="Gmail">
-            </a>
-
         </div>
 
 
         <div class="Header">
-            <p></p>
-            <img src="{{ asset('linktree-asset/ITU DIA.png') }}" class="profile" alt="logo">
-            <p></p>
-            <p><b id="Intro">@Agni.Graph</b></p>
-            <p>From Imagination to Reality</p>
+            <p>5026241099</p>
+            <p>Naufal Agni Muhammad</p>
         </div>
         <div class="MainSection">
             <div>
@@ -105,62 +94,7 @@
             </div>
 
 
-
-
-
-
-
-            <p><b>Contact us</b></p>
-
-            <div class="logo-bawah pb-5 ">
-                <img src="{{ asset('linktree-asset/logoWA.png') }}" class="logonya" alt="WA">
-                <img src="{{ asset('linktree-asset/logoIG.png') }}" class="logonya" style="width: 8%; height: auto; margin-right: 8px;" alt="IG">
-                <img src="{{ asset('linktree-asset/logoGmail.png') }}" class="logonya" style="width: 10%; height: auto;" alt="Gmail">
-            </div>
-
-        </div>
-        <div class="pb-4">
-            <a id="Tombol" href="https://bit.ly/BookPhotoEvent" target="_blank"
-                class="btn btn-light rounded-pill linkFont py-2 shadow">
-                Join Agni.Graph on Linktree
-            </a>
-        </div>
-        <div class="footer pb-5">
-            <p>Cookie Preferences • Report • Privacy • Explore</p>
-        </div>
-
-
     </div>
-
-
-    <!-- Letakkan script di sini, sebelum </body> -->
-    <script>
-        function showme() {
-            // alert("Halo Selamat Siang");
-
-            Swal.fire({
-                title: "Good job!",
-                text: "You clicked the button!",
-                icon: "success"
-            });
-            console.log("ini baris 1, sudah sampai disini");
-            a = 18 + 10;
-            console.log("Hasil Penjumlahan :" + a);
-        }
-
-        function changeText() {
-            const element = document.getElementById("Intro")
-            element.innerHTML = "FFFAAAAHHH";
-        }
-
-        function changeStyle() {
-            const element = document.getElementById("Tombol");
-            element.style.color = "red";
-            element.style.background = "yellow";
-        }
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 </body>
 
 </html>
